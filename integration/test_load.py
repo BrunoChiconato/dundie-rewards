c18803f900ad13c5057707e218a9f6ef7f719dd3
Bruno Chiconato <brunochiconato01@gmail.com>
2025-01-18 16:26:38 -0300

    Added more integration tests.

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,14 +1,27 @@
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 import pytest # type: ignore
 import os
 
 
 @pytest.mark.integration
 @pytest.mark.medium
-def test_load():
+def test_load_positive_call_load_command():
     """Test cli load command."""
     csv_path = os.path.join(os.path.dirname(__file__), "..", "tests", "assets", "people.csv")
     out = check_output(
         ["dundie", "load", csv_path]
     ).decode("utf-8").split("\n")
     assert len(out) == 2
+
+@pytest.mark.integration
+@pytest.mark.medium
+@pytest.mark.parametrize("wrong_command", ["loady", "carrega", "lad", "l0ad", "lod"])
+def test_load_negative_call_load_command_with_wrong_params(wrong_command):
+    """Test cli load command."""
+    csv_path = os.path.join(os.path.dirname(__file__), "..", "tests", "assets", "people.csv")
+    with pytest.raises(CalledProcessError) as error:
+        out = check_output(
+            ["dundie", wrong_command, csv_path]
+        ).decode("utf-8").split("\n")
+    
+    assert "status 2" in str(error.getrepr())
