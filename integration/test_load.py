9d1c6b36c30ce3de835bbf911ab6a6d80834d1db
Bruno Chiconato <brunochiconato01@gmail.com>
2025-01-18 15:51:12 -0300

    Trying to fix the CI.

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,12 +1,14 @@
 from subprocess import check_output
 import pytest # type: ignore
+import os
 
 
 @pytest.mark.integration
 @pytest.mark.medium
 def test_load():
     """Test cli load command."""
+    csv_path = os.path.join(os.path.dirname(__file__), "..", "tests", "assets", "people.csv")
     out = check_output(
-        ["dundie", "load", "tests/assets/people.csv"]
+        ["dundie", "load", csv_path]
     ).decode("utf-8").split("\n")
     assert len(out) == 2
