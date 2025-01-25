5efc0efe96ab4362afbc2fc3f6718672b57b2018
Bruno Chiconato <brunochiconato01@gmail.com>
2025-01-25 15:50:10 -0300

    Add commands - show - add - remove

--- dundie/cli.py
+++ dundie/cli.py
@@ -1,3 +1,5 @@
+import json
+
 import pkg_resources
 import rich_click as click
 from rich.console import Console
@@ -43,3 +45,52 @@ def load(filepath):
 
     console = Console()
     console.print(table)
+
+
+@main.command()
+@click.option("--dept", required=False)
+@click.option("--email", required=False)
+@click.option("--output", default=None)
+def show(output, **query):
+    """Show all employees."""
+    result = core.read(**query)
+
+    if output:
+        with open(output, "w") as output_file:
+            output_file.write(json.dumps(result, indent=4))
+        return
+
+    if not result:
+        print("No results found.")
+
+    table = Table(title="Dundler Mifflin Report")
+    for key in result[0]:
+        table.add_column(key.title(), style="cyan")
+
+    for person in result:
+        table.add_row(*[str(value) for value in person.values()])
+
+    console = Console()
+    console.print(table)
+
+
+@main.command()
+@click.argument("value", type=click.INT, required=True)
+@click.option("--dept", required=False)
+@click.option("--email", required=False)
+@click.pass_context
+def add(ctx, value, **query):
+    """Add points to employees or departments."""
+    core.add(value, **query)
+    ctx.invoke(show, **query)
+
+
+@main.command()
+@click.argument("value", type=click.INT, required=True)
+@click.option("--dept", required=False)
+@click.option("--email", required=False)
+@click.pass_context
+def remove(ctx, value, **query):
+    """Remove points to employees or departments."""
+    core.add(-value, **query)
+    ctx.invoke(show, **query)
--- dundie/core.py
+++ dundie/core.py
@@ -1,8 +1,9 @@
 """Core module of dundie"""
 
+import os
 from csv import reader
 
-from dundie.database import add_person, commit, connect
+from dundie.database import add_movement, add_person, commit, connect
 from dundie.utils.log import get_logger
 
 log = get_logger()
@@ -31,3 +32,40 @@ def load(filepath):
 
     commit(db)
     return people
+
+
+def read(dept=None, email=None):
+    db = connect()
+    results = []
+    for pk, person in db["people"].items():
+        item = {
+            "e-mail": pk,
+            "dept": person.get("department"),
+            "role": person.get("role"),
+            "name": person.get("name"),
+            "balance": db["balance"].get(pk, 0),
+            "last_movement": (
+                db["movement"][pk][-1]["date"]
+                if pk in db["movement"] and db["movement"][pk]
+                else None
+            ),
+        }
+        if dept and item["dept"] != dept:
+            continue
+        if email and item["e-mail"] != email:
+            continue
+        results.append(item)
+    return results
+
+
+def add(value, **query):
+    """Add value to each record on query."""
+    people = read(**query)
+    if not people:
+        raise RuntimeError("No records found.")
+
+    db = connect()
+    user = os.getenv("USER")
+    for person in people:
+        add_movement(db, person["e-mail"], value, user)
+    commit(db)
